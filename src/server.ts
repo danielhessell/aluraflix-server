3d5026a9552cba3f20af57fc8816979cf81c963f
Daniel Hessel <hesseldaniel21@gmail.com>
2021-07-09 22:11:41 -0300

    feat: pre routes

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import express, { NextFunction, Request, Response } from 'express';
 
 import 'express-async-errors';
 import { AppError } from './errors/AppError';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
